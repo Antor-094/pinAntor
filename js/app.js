7c65c289c3d2e9f1fd7c03894420120dc7ada3cd
Antor-094 <mdantor556677889@gmail.com>
2023-02-16 19:34:51 +0600

    Almost getting done bro

--- js/app.js
+++ js/app.js
@@ -1,23 +1,68 @@
 
-
-
 function getPin(){
-    const pin = pinGenerate();
-    const pinString = pin+'';
+    const pin = generatePin();
+    const pinString = pin+"";
     if(pinString.length===4){
-          return pinString;
+         
+        return pinString;
     }else{
-        return getPin();
+        return getPin()
     }
 }
-function pinGenerate(){
-    const random = Math.round(Math.random()*10000);
+
+function generatePin(){
+    const random = Math.round(Math.random()*10000)
     return random;
 }
 
-document.getElementById('pin-n').addEventListener('click',function(){
+document.getElementById('generate-pin').addEventListener('click',function(){
     
     const pin = getPin();
    
-    document.getElementById('pin-show').value = pin;
+   const displayPinField = document.getElementById('display-pin');
+   displayPinField.value = pin;
+})
+
+document.getElementById('calculator').addEventListener('click',function(event){
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+    if(isNaN(number)){
+        if(number==='C'){
+            typedNumberField.value = '';
+        }else if(number === '<'){
+           const antor = previousTypedNumber.split('');
+           antor.pop();
+           const antor2 = antor.join('')
+           typedNumberField.value=antor2;
+        }
+    }else{
+        
+       
+       const newTypedNumber = previousTypedNumber + number;
+
+       typedNumberField.value = newTypedNumber;
+
+    }
+})
+
+document.getElementById('submit-btn1').addEventListener('click',function(){
+    
+    const pinField = document.getElementById('display-pin');
+    const pinNumber = pinField.value;
+
+    const pinTypedNumber = document.getElementById('typed-numbers')
+    const typedNumbers = pinTypedNumber.value;
+    const pinSuccessMessage = document.getElementById('positive-reply');
+    const pinFailureMessage = document.getElementById('negative-reply');
+
+    if(pinNumber===typedNumbers){
+        
+        pinSuccessMessage.style.display = 'block';
+        pinFailureMessage.style.display = 'none';
+    }else{
+        
+        pinFailureMessage.style.display = 'block';
+        pinSuccessMessage.style.display = 'none';
+    }
 })
